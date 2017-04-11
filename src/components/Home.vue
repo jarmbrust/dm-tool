361b797f4117ab9e2064cc4d709e51e6dc4fb155
James Armbrust <james.armbrust@gmail.com>
2017-04-10 18:17:10 -0600

    typo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,7 @@
     </p>
     <br/>
     <p>
-      I just created this website here on April 8th, so it is pretty bare right now, but I will fill it out as I go here.  Most of last weekend was taken up working on the player stat tool from my D&D game.
+      I just created this website on April 8th, so it is pretty bare right now, but I will fill it out soon.  Most of last weekend was taken up working on the player stat tool from my D&D game.
     </p>
     <p>
       If you are curious, site is built utilizing the <a href="https://vuejs.org/">Vuejs</a> javascript framework (which has similarities to both React and Angular 2), and my code for it can be found on <a href="https://github.com/jarmbrust/dm-tool">GitHub</a>. Note it was called dm-tool as the original intention of the site was to build a tool for gamemastering.  It is still my intent to build out that tool, but I will instead make that part of the overall website.
