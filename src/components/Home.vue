29cf13148a103828cbe41e40a90a436c36e0335b
James Armbrust <james.armbrust@gmail.com>
2017-04-16 18:33:22 -0600

    some text changed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,17 +5,15 @@
       I am James, and this is my website.
     </p>
     <p>
-      I am a coder, gamer, writer, history buff, science buff, and polymath living with my wonderful wife and our menagerie of dogs and cats in beautiful Colorado.
+      I am a coder, gamer, writer, history buff, science buff, and self-described polymath living with my wonderful wife and our menagerie of dogs and cats in beautiful Colorado.
     </p>
     <br/>
     <p>
-      I created this website the weekend of April 9th, so still a work in progress.  I hope to have more up in the next few days but for now you can checkout the "Player Stat Tool" I made for an upcoming D&D game I'll be running.
+      I created this a few days ago, so still a work in progress.  I hope to have more up in the next few days but for now you can checkout the "Player Stat Tool" I made for an upcoming D&D game I'll be running.
     </p>
     <p>
-      If you are curious, site is built utilizing the <a href="https://vuejs.org/">Vuejs</a> javascript framework (which has similarities to both React and Angular 2), and my code for it can be found on <a href="https://github.com/jarmbrust/dm-tool">GitHub</a>. Note it was called dm-tool as the original intention of the site was to build a comprehensive tool for gamemastering.  It is still my intent to build out that tool, but I will instead make that part of the overall website and will rename the repository.
+      If you are curious, site is built utilizing the <a href="https://vuejs.org/">Vuejs</a> javascript framework (which has similarities to both React and Angular 2), and my code for it can be found on <a href="https://github.com/jarmbrust/dm-tool">GitHub</a>.
     </p>
-
-
   </div>
 </template>
 
